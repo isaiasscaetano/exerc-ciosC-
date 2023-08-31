ba8ef1cee0b77cb6b458bab9243c5ae65b2e5a24
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2023-08-31 18:06:19 -0300

    Add files via upload

---
+++ Programao.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Serialization;
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {            
+        int n1, n2, somas;
+
+        Console.WriteLine("Digite o primeiro número: ");
+         n1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+         n2 = int.Parse(Console.ReadLine());
+
+
+        somas = n1 + n2;
+
+
+        Console.WriteLine("O resultado da soma é: " + somas);
+
+
+                Console.ReadKey();
+            
+        
+    }
+}
