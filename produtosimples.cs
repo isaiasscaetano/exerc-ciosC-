64742dda8324ab1b5a3d1703e2d2ca4ff202a0fc
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2023-09-01 11:25:12 -0300

    Add files via upload

---
+++ produtosimples.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace produto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, produto;
+           
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            produto = a * b;
+
+            Console.WriteLine("prod = {0}", produto);
+
+            Console.ReadKey();
+        }
+    }
+}
