7775763e0f77aff486e4c71761902010ed8a71d0
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2023-09-01 11:52:37 -0300

    Add files via upload

---
+++ media1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media1
+{
+    internal class media1
+    {
+        static void Main(string[] args)
+        {
+            double av1, av2, media;
+            av1 = Convert.ToDouble(Console.ReadLine());
+            av2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (av1 * 3.5 + av2 * 7.6) / (3.5 + 7.5);
+            Console.WriteLine("media = {0}", media.ToString("0.00000"));
+
+            Console.ReadKey();
+        }
+    }
+}
