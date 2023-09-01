286792aa2736938b575590b21ed08998d03b7630
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2023-09-01 10:33:48 -0300

    Add files via upload

---
+++ Programaoo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+            raio = Convert.ToDouble(Console.ReadLine());
+            area = 3.14159 * Math.Pow(raio, 2);
+            Console.WriteLine("A ={0}", area.ToString("0,0000"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
