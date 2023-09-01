eac1ce30443d1383c5e3185d8b6b5ad344b790be
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2023-09-01 10:48:05 -0300

    Add files via upload

---
+++ estremafacil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estremafacil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x;
+            a = Convert.ToInt32(Console.ReadLine());
+           
+            b = Convert.ToInt32(Console.ReadLine());
+           
+            x = a + b;
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("x = {0}", x);
+            Console.WriteLine("a = {0} e b = {1} e {2}", a, b, x);
+
+            Console.ReadKey();
+           
+                
+                
+        }
+    }
+}
