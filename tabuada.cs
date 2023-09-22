f51c2e2df290a619d6346c9354fe02517c1b4289
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2023-09-22 11:29:37 -0300

    Add files via upload

---
+++ tabuada.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, q, lim;
+
+
+        Console.Write("digite um numero");
+        q = Convert.ToInt32(Console.ReadLine());
+        Console.Write("qual e o seu limite");
+        lim = Convert.ToInt32(Console.ReadLine());
+        n = 1;
+        while (n <= lim)
+        {
+            Console.Write("{0} x {1} = {2}\n", q, n, q * n);
+            n++;
+
+
+        }
+
+        Console.Write("\n------- while -------\n");
+        n = 1;
+        while (n <= 5){
+            Console.Write("{0\n}", n);
+            n++;
+        }
+
+        Console.WriteLine("\n------- d0 -------\n");
+        n = 1;
+        do{
+            Console.Write("{0}\n", n);
+            n++;
+
+
+        } while (n <= 5);
+
+       
+
+
+
+    }
+}
